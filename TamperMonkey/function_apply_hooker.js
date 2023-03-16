78c36568b8e9bfcbe93e943f50f803882cee322b
cloudchamber <jizon0123@protonmail.com>
2023-03-16 12:08:44 +0900

    [feat] add stacktrace, clarify the expressions

--- TamperMonkey/function_apply_hooker.js
+++ TamperMonkey/function_apply_hooker.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        https://*
+// @match        *
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=mozilla.org
 // @grant        none
 // @run-at       document-start
@@ -18,8 +18,17 @@
 
     window.Function.prototype.apply = function (caller, ...args) {
         const ret = this.__apply(caller, ...args);
-        console.log("[Func Apply]", caller ?? this, args, ret);
+        console.log("[+] Func Apply Called!");
+        console.log("    [!] context => ", caller ?? "<null>");
+        console.log("    [!] func => ", this);
+        console.log("    [!] args => ", args);
+        console.log("    [!] ret => ", ret);
+
+        var stk = (new Error()).stack.split('\n').splice(1).map(elem => elem.replaceAll('    at ', ''));
+        console.log("    [!] trace");
+        for (const s of stk) {
+            console.log("        -> ", s);
+        }
         return ret;
     }
-    // Your code here...
 })();
